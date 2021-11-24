55e2533d16ba7c9ba9f55fcae5e6e3729633c430
ntospa21 <tommyaek@gmail.com>
2021-11-24 19:56:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Plot from 'react-plotly.js';
+import './sec.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+function App(){
+         return(
+        <div className="container">
+              <div className='koumpaki' >
+   </div>
+  
+
+            <h5>What are the issues which made you vote in the recent European elections? Firstly? Any others?</h5>
+            <Plot className='content'
+            data={[
+                {
+                type: 'bar',
+                title: "Share of committee chairs by political group",
+                y: ['EPP', 'S&D', 'ECR', 'ALDE', 'GREENS/EFA', 'GUE/NGL', 'EFDD', 'ENF', 'NI'],
+                x:[29.2,25.2,9.6,9.0,6.9,6.8,5.9,4.8,2.6],
+                orientation:'h',
+                groupnorm:'percent',
+                marker: {
+                color: ['grey', 'red', 'darkblue', 'yellow', 'darkgreen', 'lightbrown','cyan', 'orange', 'pink'],
+                    width: 1
+            },
+                }
+            ]}
+            layout={{width:800, height:500, margin:500,  orientation: 'h' }}
+            
+            
+            />   <div className='deutero'>    
+             <h5> Share of committee chairs by political group</h5>
+             <Plot className='content'
+            data={[
+                {
+                type: "pie",
+                labels: ["EPP", "S&D", "ECR", "ALDE", "GREENS/EFA", "GUE/NGL", "EFDD", "ENF", "NI"],
+                values: [29.2,25.2,9.6,9.0,6.9,6.8,5.9,4.8,2.6],
+                domain:{column: 0},
+                name: 'Share of committee chairs by political group',
+                hoverinfo: 'label+percent+name',
+                hole: .55 ,
+                   color: ['grey', 'red', 'darkblue', 'yellow', 'darkgreen', 'lightbrown','cyan', 'orange', 'pink'],
+                    width: 1
+            },
+                
+            ]}
+            layout={{margin:{"l": 0, "r": 0, "b": 0, "t": 0}} }
+            
+            
+            />
+          </div> 
+            <div className='trito'> 
+            <h5> Size of political groups</h5>
+            <Plot className='content'
+            data={[
+                {
+                type: 'bar',
+              
+                y: ['NI', 'ENF', 'EFDD', 'GUE/NGL', 'GREENS/EFA',  'ALDE','ECR', 'S&D', 'EPP'],
+                x:[20,36,44,51,52,68,72,189,219],
+                orientation:'h',
+                marker: {
+                color: ['grey', 'red', 'darkblue', 'yellow', 'darkgreen', 'lightbrown','cyan', 'orange', 'pink'],
+                    width: 1
+            },
+                }
+            ]}
+            layout={{width:800, height:500, margin:500, orientation: 'h' }}
+            
+            
+            />
+            
+            </div>
+       
+        </div>
+          )
+   }
 export default App;
