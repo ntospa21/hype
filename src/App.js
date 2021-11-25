4d0a51aeb9a6bfa22904f4f5509748e8ecb8f1ac
ntospa21 <tommyaek@gmail.com>
2021-11-25 16:05:58 +0200

    almost

--- src/App.js
+++ src/App.js
@@ -11,14 +11,23 @@ import theup from './assets/The european parliament members.svg'
 import age from './assets/Age of MEPs by member stateThere is an average age of MEPs, together with the maximum and minimum age, per Member State. The average age of Members on.svg'
 import mepage from './assets/MEP age icon.svg'
 import aveg from './assets/aver.svg'
+import work from './assets/The work of the european parliament.svg'
+import mission from './assets/Mission of EP icon.svg'
+import citi from './assets/Working with citizens.svg'
+import iconciti from './assets/Icon citizens.svg'
+import participate from './assets/Participate in programs that help you find a job or learn a  new oneThe Employment committee is responsible for monitoring employment and employment p.svg'
+import happy from './assets/Icon happy.svg'
+import outlook from './assets/Outlook.png'
 function App(){
 
   const [toggle, setToggle] = useState(false)
-  const [text , setText] = useState('the average')
+  
   const toggler = () =>{
     toggle ? setToggle(false)  : setToggle(true)  ;
   }
 
+ 
+  
  
  
          return(
@@ -75,14 +84,46 @@ function App(){
             onClick={(...data)=> console.log(data)}
             
             />  
-            <div>
-                        <button className="btn" style={{marginTop:'10vh', marginLeft:'95vh'}} value={text}   onClick={toggler} >  {text}</button>
+          
+            <img src={age} alt='' style={{marginLeft:'35vh', marginTop:'10vh'}}/>
+            {toggle ? <div >
+                        <button className="btn" style={{ marginLeft:'85vh' }}   onClick={toggler} > the average </button>
+                        <div style={{marginLeft:'86vh'}}>
+<img src={mepage} alt='' style={{marginLeft:'5vh'}}/> </div></div>: 
+<div  >
+                        <button className="btn" style={{ marginLeft:'85vh'}}    onClick={toggler} >   the younger</button>
+                        <div style={{marginLeft:'86vh'}}>
+<img src={aveg} alt='' style={{marginLeft:'5vh'}}/></div> </div>}
+
+<img src={work} alt='' style={{marginTop:'10vh', marginLeft:'35vh'}}/> 
+<p style={{marginLeft:'96vh', fontSize:28}}>EP's work  examples:
+</p>
+
+<p style={{marginLeft:'100vh', fontSize:28}}>Passing EU laws{<button className='btn' onClick={()=>alert('do sth')}>Read more</button>}
+</p>
+
+<p style={{marginLeft:'69vh', fontSize:28}}>Democratic scrunity of all EU Institutions{<button className='btn' onClick={()=>alert('do sth')}>Read more</button>}
+</p>
+
+
+<img src={mission} alt='' style={{marginLeft:'40vh'}}/>
+<img src={citi} alt='' style={{marginLeft:'40vh', marginTop:'15vh'}}/>
+<div>
+<button className='btn' style={{marginLeft:'108vh', marginTop:'-10vh'}}>Read more</button>
+
+</div>
+<div>
+<img src={iconciti} alt='' style={{marginLeft:'50vh', marginTop:'-15vh'}} />
+</div>
+<img src={participate} alt='' style={{marginLeft:'40vh'}}/>
+<button className='btn' style={{marginRight:'70vh', marginTop:'-10vh'}}>Read more</button>
+<img src={happy} alt='' style={{marginLeft:'40vh'}}/>
+<img src={outlook} alt='' style={{marginLeft:'10vh'}}/>
+
+
+
 </div>
 
-            <img src={age} alt='' style={{marginLeft:'35vh', marginTop:'15vh'}}/>
-            {toggle ?<img src={mepage} alt='' style={{marginLeft:'5vh'}}/> : <img src={aveg} alt='' style={{marginLeft:'5vh'}}/>}
-           
-        </div>
           )
    }
 export default App;
